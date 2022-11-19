72b56d2cf48ba924e22bb2b771f9aa6fbb08ac7a
josheligoldman <josheligoldman@gmail.com>
2022-11-19 10:58:44 -0500

    Miss Information code added

---
+++ miss_information/scripts/content.js
@@ -0,0 +1,68 @@
+var tweets, child, text_content, badge, icon, mutationObserver, our_icons;
+
+const text_child_node_indices = [0, 0, 0, 1, 1, 1, 0];
+const badge_child_node_indices = [0, 0, 0, 1, 1, 1];
+
+console.log("Content");
+
+const body_element = document.querySelector('body');
+
+function mutation_callback(mutations) {
+    mutationObserver.disconnect();
+    console.log("Mutation Observed");
+    tweets = document.querySelectorAll('article');
+    console.log("Tweets");
+    console.log(tweets);
+
+    our_icons = document.querySelectorAll('#our_icons');
+
+    for (let i = 0; i < our_icons.length; i++) {
+        our_icons[i].remove();
+    }
+
+    for (let i = 0; i < tweets.length; i++) {
+        text_child = tweets[i];
+        badge_child = tweets[i];
+
+        for (let j = 0; j < text_child_node_indices.length; j++) {
+            text_child = text_child.childNodes[text_child_node_indices[j]];
+        }
+
+        for (let j = 0; j < badge_child_node_indices.length; j++) {
+            badge_child = badge_child.childNodes[badge_child_node_indices[j]];
+        }
+        badge_child = badge_child.lastChild.firstChild;
+
+        console.log(i.toString());
+        text_content = text_child.textContent;
+        console.log("Text Content");
+        console.log(text_content);
+        console.log("Badge Child");
+        console.log(badge_child);
+
+        badge = document.createElement('div');
+        badge.id = "our_icons";
+        badge.classList.add('css-1dbjc4n', 'r-18u37iz', 'r-1h0z5md')
+
+        icon = document.createElement('img');
+        icon.src = 'https://iili.io/HHHg6y7.png';
+        badge.appendChild(icon);
+        badge_child.appendChild(badge);
+    }
+    mutationObserver = new MutationObserver(mutation_callback);
+
+    mutationObserver.observe(
+        body_element,
+        {subtree: true, childList: true}
+    );
+
+}
+
+var mutationObserver = new MutationObserver(mutation_callback);
+
+mutationObserver.observe(
+    body_element,
+    {subtree: true, childList: true}
+)
+
+
